e0a066dc3af4fbc1a44710db462f125156c84fd4
Alexpra2 <alexpra2@gmail.com>
2024-12-09 22:11:22 +0100

    mejoras finales

--- js/link.js
+++ js/link.js
@@ -2,52 +2,86 @@ const guardarBtn = document.getElementById('guardarBtn');
 
 //localStorage.clear();
 console.log(localStorage);
+
 cargarEnPantalla();
+
 function cargarEnPantalla() {
 
     const contenedor = document.getElementById('ulContenedorGuardado');
-    let mislink = JSON.parse(localStorage.getItem('mislink')) || []; 
-    
     contenedor.innerHTML = '';
     
-    mislink.forEach(enlace => {
+    let mislink = localStorage.getItem('mislink');
+
+    if (mislink) {
+        mislink = JSON.parse(mislink);
+    }
+    else {
+        mislink = [];
+    }
+
+    mislink.forEach((link,i) => {
         const li = document.createElement('li');
         li.innerHTML = `
-        <a href="${enlace.url}" target="_blank">${enlace.nombre}</a>
+        <a href="${link.url}" target="_blank">${link.nombre}</a>
+        <button class="eliminarBtn" data-index="${i}">X</button>
         `;
         contenedor.appendChild(li);
-    
-}); 
 
+    });
+    
+    const eliminarBtn = document.querySelectorAll('.eliminarBtn')
+    eliminarBtn.forEach((button) => {
+        button.addEventListener('click', eliminarLink);
+    });
 }
+
+function eliminarLink(event) {
+    const index = event.target.dataset.index; 
+    let mislink = localStorage.getItem('mislink');
+
+    if (mislink) {
+        mislink = JSON.parse(mislink);
+    }
+    else {
+        mislink = [];
+    }
+    
+    mislink.splice(index, 1); 
+    localStorage.setItem('mislink', JSON.stringify(mislink)); // Actualiza el localStorage
+
+    cargarEnPantalla();                      // Vuelve a cargar la lista
+}
+
 
 guardarBtn.addEventListener('click', () => {
 
     const datonombre = document.getElementById('nombre').value;
     const datourl = document.getElementById('url').value;
-    
+
     //console.log(nombre);
     //console.log(url);
-    
+
     let link = {
-    nombre: datonombre,  
-    url: datourl  
-    };    
-    
+        nombre: datonombre,
+        url: datourl
+    };
+
     let mislink = localStorage.getItem('mislink');
-        
+
     if (mislink) {
         mislink = JSON.parse(mislink);
-    }     
+    }
     else {
         mislink = [];
     }
-       
+
     mislink.push(link);
     localStorage.setItem('mislink', JSON.stringify(mislink));
-    
+
     console.log(localStorage.getItem('mislink'));
-    
+
+    document.getElementById('nombre').value = '';
+    document.getElementById('url').value = '';
+
     cargarEnPantalla()
-    
 });
